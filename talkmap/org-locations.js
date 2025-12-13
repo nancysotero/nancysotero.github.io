8e092b4a84401f2bdf0f55442e9a3a144b5f478f
nancysotero <88988047+nancysotero@users.noreply.github.com>
2025-12-13 16:07:12 +0100

    Update event names to remove HTML line breaks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,87 +1,87 @@
 var addressPoints = [
   [
-    "15th Congress of the Latin American Society of Neuropsychology, 2017<br />Natal, Brazil",
+    "15th Congress of the Latin American Society of Neuropsychology, 2017 - Natal, Brazil",
     -5.8757673,
-    -35.1799309
+    -35.1799309,15.76
   ],
   [
-    "XXIV National Meeting of Speech Therapy Students, 2017 (organization and presentation)<br />João Pessoa, Brazil",
+    "XXIV National Meeting of Speech Therapy Students, 2017 (organization and presentation) - João Pessoa, Brazil",
     -7.1377485,
-    -34.8458974
+    -34.8458974,17
   ],
   [
-    "XXV National Meeting of Speech Therapy Students, 2018 (organization)<br />São Paulo, Brazil",
+    "XXV National Meeting of Speech Therapy Students, 2018 (organization) - São Paulo, Brazil",
     -23.5981078,
-    -46.6442493
+    -46.6442493,16
   ],
   [
-    "XXVI National Meeting of Speech Therapy Students, 2019 (organization and presentation)<br />Recife, Brazil",
+    "XXVI National Meeting of Speech Therapy Students, 2019 (organization and presentation) - Recife, Brazil",
     -8.0505815,
-    -34.9481354
+    -34.9481354,18
   ],
   [
-    "Neuroscience and Cognition Course at UFABC, 2018 (summer course)<br />UFABC, São Bernardo, Brazil",
+    "Neuroscience and Cognition Course at UFABC, 2018 (summer course) - UFABC, São Bernardo, Brazil",
     -23.6780399,
-    -46.5635019
+    -46.5635019,16
   ],
   [
-    "VIII Neuroscience Course at UFRGS, 2017 (summer course)<br />UFRGS, Porto Alegre, Brazil",
+    "VIII Neuroscience Course at UFRGS, 2017 (summer course) - UFRGS, Porto Alegre, Brazil",
     -30.0339726,
-    -51.2190483
+    -51.2190483,16
   ],
   [
-    "XXVI Brazilian Speech Therapy Congress, 2018 (student's representation)<br />Curitiba, Brazil",
+    "XXVI Brazilian Speech Therapy Congress, 2018 (student's representation) - Curitiba, Brazil",
     -25.4250443,
-    -49.3212071
+    -49.3212071,17
   ],
   [
-    "XXVII Brazilian Speech Therapy Congress, 2019 (talk)<br />Belo Horizonte, Brazil",
+    "XXVII Brazilian Speech Therapy Congress, 2019 (talk) - Belo Horizonte, Brazil",
     -19.870682,
-    -43.9677359
+    -43.9677359,17
   ],
   [
-    "VIII and IX Neuroengineering Symposium, 2022 and 2023 (organization and presentations)<br />Macaíba, Brazil",
+    "VIII and IX Neuroengineering Symposium, 2022 and 2023 (organization and presentations) - Macaíba, Brazil",
     -5.8903993,
-    -35.3915359
+    -35.3915359,17
   ],
   [
-    "5th workshop on Cognitive neuroscience of auditory and cross-modal perception, 2024 (presentation)<br />Košice, Slovakia",
+    "5th workshop on Cognitive neuroscience of auditory and cross-modal perception, 2024 (presentation) - Košice, Slovakia",
     48.7223961,
-    21.2414861
+    21.2414861,14
   ],
   [
-    "Ecole Polytechnique Fédérale de Lausanne, 2022 (internship)<br />Geneve, Switzerland",
+    "Ecole Polytechnique Fédérale de Lausanne, 2022 (internship) - Geneve, Switzerland",
     46.2220851,
-    6.1482666
+    6.1482666,17
   ],
   [
-    "FENS Forum, 2024<br />Vienna, Austria",
+    "FENS Forum, 2024 - Vienna, Austria",
     48.2196447,
-    16.3475329
+    16.3475329,17
   ],
   [
-    "Research Center for Natural Sciences, 2024 (internship)<br />Budapest, Hungary",
+    "Research Center for Natural Sciences, 2024 (internship) -Budapest, Hungary",
     47.4740534,
-    19.0601823
+    19.0601823,17
   ],
   [
-    "European Summer School on Eye Movements<br />Athens, Greece",
+    "European Summer School on Eye Movements - Athens, Greece",
     37.9800779,
-    23.7340649
+    23.7340649,16
   ],
   [
-    "10th International Symposium on Auditory and Audiological Research, 2025<br />Nyborg, Denmark",
+    "10th International Symposium on Auditory and Audiological Research, 2025 - Nyborg, Denmark",
     55.3179884,
-    10.8109716
+    10.8109716,17
   ],
   [
-    "47th European Conference on Visual Perception, 2025<br />Mainz, Germany",
+    "47th European Conference on Visual Perception, 2025 - Mainz, Germany",
     49.9926403,
-    8.2415552
+    8.2415552,17
   ],
   [
-    "8th International Conference on Auditory Cortex, 2025<br />Maastricht, The Netherlands",
+    "8th International Conference on Auditory Cortex, 2025 - Maastricht, The Netherlands",
     50.8470175,
-    5.697484
+    5.697484,18
   ]
 ];
