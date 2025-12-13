ffbba4c28dc82e6c66e0284f3dea758c625c60ca
nancysotero <88988047+nancysotero@users.noreply.github.com>
2025-12-13 15:46:02 +0100

    Clean up addressPoints data structure
    Removed unnecessary values from latitude and longitude entries.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -2,86 +2,86 @@ var addressPoints = [
   [
     "15th Congress of the Latin American Society of Neuropsychology, 2017<br />Natal, Brazil",
     -5.8757673,
-    -35.1799309,15.76
+    -35.1799309
   ],
   [
     "XXIV National Meeting of Speech Therapy Students, 2017 (organization and presentation)<br />João Pessoa, Brazil",
     -7.1377485,
-    -34.8458974,17
+    -34.8458974
   ],
   [
     "XXV National Meeting of Speech Therapy Students, 2018 (organization)<br />São Paulo, Brazil",
     -23.5981078,
-    -46.6442493,16
+    -46.6442493
   ],
   [
     "XXVI National Meeting of Speech Therapy Students, 2019 (organization and presentation)<br />Recife, Brazil",
     -8.0505815,
-    -34.9481354,18
+    -34.9481354
   ],
   [
     "Neuroscience and Cognition Course at UFABC, 2018 (summer course)<br />UFABC, São Bernardo, Brazil",
     -23.6780399,
-    -46.5635019,16
+    -46.5635019
   ],
   [
     "VIII Neuroscience Course at UFRGS, 2017 (summer course)<br />UFRGS, Porto Alegre, Bazil",
     -30.0339726,
-    -51.2190483,16
+    -51.2190483
   ],
   [
     "XXVI Brazilian Speech Therapy Congress, 2018 (student's representation)<br />Curitiba, Brazil",
     -25.4250443,
-    -49.3212071,17
+    -49.3212071
   ]
   [
     "XXVII Brazilian Speech Therapy Congress, 2019 (talk)<br />Belo Horizonte, Brazil",
     -19.870682,
-    -43.9677359,17
+    -43.9677359
   ],
   [
     "VIII and IX Neuroengineering Symposium, 2022 and 2023 (organization and presentations)<br />Macaíba, Brazil",
     -5.8903993,
-    -35.3915359,17
+    -35.3915359
   ],
   [
     "5th workshop on Cognitive neuroscience of auditory and cross-modal perception, 2024 (presentation)<br />Košice, Slovakia",
     48.7223961,
-    21.2414861,14
+    21.2414861
   ],
   [
     "Ecole Polytechnique Fédérale de Lausanne, 2022 (internship)<br />Geneve, Switzerland",
     46.2220851,
-    6.1482666,17
+    6.1482666
   ],
   [
     "FENS Forum, 2024<br />Vienna, Austria",
     48.2196447,
-    16.3475329,17
+    16.3475329
   ],
   [
     "Research Center for Natural Sciences, 2024 (internship)<br />Budapest, Hungary",
     47.4740534,
-    19.0601823,17
+    19.0601823
   ],
   [
     "European Summer School on Eye Movements<br />Athens, Greece",
     37.9800779,
-    23.7340649,16
+    23.7340649
   ],
   [
     "10th International Symposium on Auditory and Audiological Research, 2025<br />Nyborg, Denmark",
     55.3179884,
-    10.8109716,17
+    10.8109716
   ],
   [
     "47th European Conference on Visual Perception, 2025<br />Mainz, Germany",
     49.9926403,
-    8.2415552,17
+    8.2415552
   ],
   [
     "8th International Conference on Auditory Cortex, 2025<br />Maastricht, The Netherlands",
     50.8470175,
-    5.697484,18
+    5.697484
   ]
 ];
